b481b2cc7f5ed04133bbfe2fdff77a1fedfc2d3b
mbelowwww <mbelowwww@yandex.ru>
2020-11-09 18:05:43 +0400

    add menu

---
+++ app/src/main/java/com/example/calculation/FinalHeightActivity.java
@@ -0,0 +1,106 @@
+package com.example.calculation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+
+import service.CalculationService;
+
+public class FinalHeightActivity extends AppCompatActivity {
+
+    private boolean isPlus = false;
+
+    private BigDecimal heightMeterValue = BigDecimal.ZERO;
+
+    private BigDecimal heightAirMeterValue = BigDecimal.ZERO;
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_heigh);
+        EditText heightFeet = (EditText) findViewById(R.id.heightFeet);
+        EditText heightAirMeters = (EditText) findViewById(R.id.heightAeroMeter);
+
+
+        heightAirMeters.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if (s.length() > 0) {
+                    heightAirMeterValue = BigDecimal.valueOf(Double.parseDouble(s.toString()));
+                }
+            }
+        });
+
+        TextView heightMeter = (TextView) findViewById(R.id.heightMeters);
+        Button resultBtn = (Button) findViewById(R.id.resultBtn);
+
+        heightFeet.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @SuppressLint("SetTextI18n")
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if (s.length() > 0) {
+                    BigDecimal heightFeetValue = BigDecimal.valueOf(Double.parseDouble(s.toString()));
+                    heightMeterValue = CalculationService.feetToMeters(heightFeetValue);
+                    heightMeter.setText(heightMeterValue.toPlainString() + " м.");
+                } else {
+                    heightMeter.setText("");
+                }
+            }
+        });
+
+        Button plus = (Button) findViewById(R.id.plusBtn);
+
+        Button minus = (Button) findViewById(R.id.minusBtn);
+
+        plus.setOnClickListener(v -> {
+            isPlus = true;
+            plus.setEnabled(false);
+            minus.setEnabled(true);
+        });
+
+        minus.setOnClickListener(v -> {
+            isPlus = false;
+            minus.setEnabled(false);
+            plus.setEnabled(true);
+        });
+
+        TextView resultText = (TextView) findViewById(R.id.resultText);
+
+        resultBtn.setOnClickListener(v -> {
+            BigDecimal totalHeight = CalculationService.getTotalHeight(heightMeterValue, heightAirMeterValue, isPlus);
+            resultText.setText(totalHeight.toPlainString() + " м.");
+        });
+
+    }
+
+    public void heightFeetClick(View view) {
+
+    }
+}
--- app/src/main/java/com/example/calculation/MainActivity.java
+++ app/src/main/java/com/example/calculation/MainActivity.java
@@ -1,106 +1,26 @@
 package com.example.calculation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.math.BigDecimal;
-
-import service.CalculationService;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean isPlus = false;
-
-    private BigDecimal heightMeterValue = BigDecimal.ZERO;
-
-    private BigDecimal heightAirMeterValue = BigDecimal.ZERO;
-
-
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText heightFeet = (EditText) findViewById(R.id.heightFeet);
-        EditText heightAirMeters = (EditText) findViewById(R.id.heightAeroMeter);
-
-        heightAirMeters.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                if (s.length() > 0) {
-                    heightAirMeterValue = BigDecimal.valueOf(Double.parseDouble(s.toString()));
-                }
-            }
-        });
-
-        TextView heightMeter = (TextView) findViewById(R.id.heightMeters);
-        Button resultBtn = (Button) findViewById(R.id.resultBtn);
-
-        heightFeet.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            @SuppressLint("SetTextI18n")
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                if (s.length() > 0) {
-                    BigDecimal heightFeetValue = BigDecimal.valueOf(Double.parseDouble(s.toString()));
-                    heightMeterValue = CalculationService.feetToMeters(heightFeetValue);
-                    heightMeter.setText(heightMeterValue.toPlainString() + " м.");
-                } else {
-                    heightMeter.setText("");
-                }
-            }
-        });
-
-
-        Button plus = (Button) findViewById(R.id.plusBtn);
-
-        Button minus = (Button) findViewById(R.id.minusBtn);
-
-        plus.setOnClickListener(v -> {
-            isPlus = true;
-            plus.setEnabled(false);
-            minus.setEnabled(true);
-        });
-
-        minus.setOnClickListener(v -> {
-            isPlus = false;
-            minus.setEnabled(false);
-            plus.setEnabled(true);
-        });
-
-        TextView resultText = (TextView) findViewById(R.id.resultText);
-
-        resultBtn.setOnClickListener(v -> {
-            BigDecimal totalHeight = CalculationService.getTotalHeight(heightMeterValue, heightAirMeterValue, isPlus);
-            resultText.setText(totalHeight.toPlainString() + " м.");
-        });
-
     }
 
-    public void heightFeetClick(View view) {
-
+    public void onClickFinalHeightActivityBtn(View view) {
+        Intent intent = new Intent(MainActivity.this, FinalHeightActivity.class);
+        startActivity(intent);
     }
 }
